9ac720dde623ebbf538ab8ea15be3ea0aba80ccb
Swastik Panda <swastiksuman@gmail.com>
2021-03-01 22:35:39 +0530

    bolt views

--- Bolts/index.js
+++ Bolts/index.js
@@ -17,3 +17,38 @@ const app = new App({
     console.log(message);
     await say(`Hey there <@${message.user}>!`);
   });
+
+  app.event('app_home_opened', async ({ event, client }) => {
+    try {
+      // Call views.publish with the built-in client
+      const result = await client.views.publish({
+        // Use the user ID associated with the event
+        user_id: event.user,
+        view: {
+          // Home tabs must be enabled in your app configuration page under "App Home"
+          "type": "home",
+          "blocks": [
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "*Welcome home, <@" + event.user + "> :house:*"
+              }
+            },
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>."
+              }
+            }
+          ]
+        }
+      });
+  
+      console.log(result);
+    }
+    catch (error) {
+      console.error(error);
+    }
+  });
